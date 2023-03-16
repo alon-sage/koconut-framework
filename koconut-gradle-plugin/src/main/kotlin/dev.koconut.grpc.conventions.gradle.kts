e7cbc235489e7fa8cc1dd558392ae4d503500ec2
Ivan Babintsev <alon.sage@gmail.com>
2023-03-16 23:01:09 +0400

    Fix plugins to work with any project

---
+++ koconut-gradle-plugin/src/main/kotlin/dev.koconut.grpc.conventions.gradle.kts
@@ -0,0 +1,56 @@
+import com.google.protobuf.gradle.ProtobufExtension
+import org.gradle.kotlin.dsl.findByType
+import org.jetbrains.kotlin.gradle.utils.loadPropertyFromResources
+
+plugins {
+    id("dev.koconut.conventions")
+}
+
+with(dependencies) {
+    configurations.all {
+        when (name) {
+            "api" -> {
+                add(name, "io.grpc:grpc-protobuf")
+                add(name, "io.grpc:grpc-kotlin-stub")
+                add(name, "com.google.protobuf:protobuf-kotlin")
+            }
+        }
+    }
+}
+
+val koconutBom = dependencies.platform(loadPropertyFromResources("bom-spec.properties", "koconut-bom"))
+val protoc = dependencies.create("com.google.protobuf", "protoc")
+val protocGrpc = dependencies.create("io.grpc", "protoc-gen-grpc-java")
+val protocGrpcKotlin = dependencies.create("io.grpc", "protoc-gen-grpc-kotlin")
+
+val protobufArtifacts: Set<ResolvedDependency> = configurations
+    .detachedConfiguration(koconutBom, protoc, protocGrpc, protocGrpcKotlin)
+    .resolvedConfiguration
+    .firstLevelModuleDependencies
+
+afterEvaluate {
+    extensions
+        .findByType<ProtobufExtension>()
+        ?.apply {
+            protoc {
+                artifact = protobufArtifacts.single { it.module.id.module == protoc.module }.name
+            }
+            plugins {
+                create("grpc") {
+                    artifact = protobufArtifacts.single { it.module.id.module == protocGrpc.module }.name
+                }
+                create("grpcKotlin") {
+                    artifact = protobufArtifacts.single { it.module.id.module == protocGrpcKotlin.module }.name + ":jdk8@jar"
+                }
+            }
+            generateProtoTasks.all().configureEach {
+                plugins {
+                    create("grpc")
+                    create("grpcKotlin")
+                }
+                builtins {
+                    create("kotlin")
+                }
+            }
+        }
+}
