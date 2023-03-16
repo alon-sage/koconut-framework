25cc26a71a75a4f15e807a933e05ccc70c23180e
Ivan Babintsev <alon.sage@gmail.com>
2023-03-16 22:27:35 +0400

    Update dependencies versions

--- build-tools/build.gradle.kts
+++ build-tools/build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    api(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    api(platform("org.jetbrains.kotlin:kotlin-bom:1.8.10"))
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
 }
 
 tasks.withType<KotlinCompile> {
--- koconut-dependencies/build.gradle.kts
+++ koconut-dependencies/build.gradle.kts
@@ -13,16 +13,16 @@ dependencies {
     api(platform("org.jetbrains.kotlin:kotlin-bom:${getKotlinPluginVersion()}"))
     api(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.4"))
     api(platform("com.google.inject:guice-bom:5.1.0"))
-    api(platform("io.netty:netty-bom:4.1.85.Final"))
-    api(platform("org.apache.logging.log4j:log4j-bom:2.19.0"))
-    api(platform("com.fasterxml.jackson:jackson-bom:2.14.1"))
-    api(platform("com.google.protobuf:protobuf-bom:3.21.12"))
-    api(platform("io.grpc:grpc-bom:1.51.1"))
-    api(platform("io.ktor:ktor-bom:2.2.1"))
-    api(platform("io.micrometer:micrometer-bom:1.10.2"))
-    api(platform("io.opentelemetry:opentelemetry-bom:1.20.1"))
+    api(platform("io.netty:netty-bom:4.1.90.Final"))
+    api(platform("org.apache.logging.log4j:log4j-bom:2.20.0"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.14.2"))
+    api(platform("com.google.protobuf:protobuf-bom:3.22.2"))
+    api(platform("io.grpc:grpc-bom:1.53.0"))
+    api(platform("io.ktor:ktor-bom:2.2.4"))
+    api(platform("io.micrometer:micrometer-bom:1.10.5"))
+    api(platform("io.opentelemetry:opentelemetry-bom:1.24.0"))
     api(platform("org.testcontainers:testcontainers-bom:1.17.6"))
-    api(platform("org.junit:junit-bom:5.9.1"))
+    api(platform("org.junit:junit-bom:5.9.2"))
 
     constraints {
         api("com.google.auto.service:auto-service-annotations:1.0.1")
@@ -30,25 +30,23 @@ dependencies {
 
         api("com.typesafe:config:1.4.2")
 
-        api("com.github.ajalt.clikt:clikt:3.5.0")
+        api("com.github.ajalt.clikt:clikt:3.5.2")
 
-        api("org.slf4j:slf4j-api:2.0.5")
-        api("org.slf4j:slf4j-simple:2.0.5")
-        api("org.slf4j:jul-to-slf4j:2.0.5")
-        api("net.logstash.logback:logstash-logback-encoder:7.2")
-        api("org.codehaus.janino:janino:3.1.9")
-        api("ch.qos.logback:logback-core:1.4.5")
-        api("ch.qos.logback:logback-classic:1.4.5")
+        api("org.slf4j:slf4j-api:2.0.6")
+        api("org.slf4j:slf4j-simple:2.0.6")
+        api("org.slf4j:jul-to-slf4j:2.0.6")
+        api("ch.qos.logback:logback-core:1.4.6")
+        api("ch.qos.logback:logback-classic:1.4.6")
 
         api("org.spekframework.spek2:spek-dsl-jvm:2.0.19")
         api("org.spekframework.spek2:spek-runner-junit5:2.0.19")
-        api("io.mockk:mockk:1.13.3")
+        api("io.mockk:mockk:1.13.4")
 
-        api("com.google.protobuf:protoc:3.21.12")
-        api("com.google.protobuf:protobuf-kotlin:3.21.12")
+        api("com.google.protobuf:protoc:3.22.2")
+        api("com.google.protobuf:protobuf-kotlin:3.22.2")
         api("io.grpc:grpc-kotlin-stub:1.3.0")
         api("io.grpc:protoc-gen-grpc-kotlin:1.3.0")
-        api("com.google.protobuf:protobuf-gradle-plugin:0.9.1")
+        api("com.google.protobuf:protobuf-gradle-plugin:0.9.2")
     }
 }
 
